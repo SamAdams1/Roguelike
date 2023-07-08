5b535209c61f06042159f2c87b3bd1bc48573991
Sam <sammyadams04@gmail.com>
2023-07-07 22:29:32 -0400

    added toggle shooting and camera

--- src/Scripts/Player.gd
+++ src/Scripts/Player.gd
@@ -6,30 +6,37 @@ var speed = 200
 #attacking
 var bullet = preload('res://Scenes/bullet.tscn')
 var waitToFire = false
-var fireRate = 0.5
+var toggleFire = false
+var fireRate = 1
 var bulletSpeed = 500
+
 
 
 func _physics_process(_delta):
 	handle_input()
+	
+func _input(event): #shooting has to be in here so only one input is taken per mouse click
+	if event.is_action_pressed("attack"):
+		toggleFire = !toggleFire
+		
+	while toggleFire and !waitToFire:
+		waitToFire = true
+		fire()
+		yield(get_tree().create_timer(fireRate), "timeout")
+		waitToFire = false
 	
 	
 func handle_input():
 	#movement
 	velocity.x = int(Input.is_action_pressed("right")) - int(Input.is_action_pressed("left"))
 	velocity.y = int(Input.is_action_pressed("down")) - int(Input.is_action_pressed("up"))
+	
+	
 	#fixes double speed on diagonals
 	velocity = velocity.normalized()
 	velocity = move_and_slide(velocity * speed)
 	
-	
-	
 	look_at(get_global_mouse_position())
-	if Input.is_action_pressed("attack") and !waitToFire:
-		fire()
-		waitToFire = true
-		yield(get_tree().create_timer(fireRate), "timeout")
-		waitToFire = false
 
 func fire():
 	var bullet_instance = bullet.instance()
