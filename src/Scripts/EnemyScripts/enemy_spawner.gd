0aeec739fbc0d7c2a1792166550b808b1c1f7cd4
SoccerSam16 <119465691+SoccerSam16@users.noreply.github.com>
2023-07-12 18:43:37 -0400

    Revert "merge conflict"
    This reverts commit d57cbdfeb49f2b39013ddfa24fca6162736cb76a, reversing
    changes made to 1498c77ff94439f970ad9674ed345b24f797d819.

--- src/Scripts/EnemyScripts/enemy_spawner.gd
+++ src/Scripts/EnemyScripts/enemy_spawner.gd
@@ -2,17 +2,10 @@ extends Node
 
 var enemy_asteroid = preload("res://Scenes/Enemies/AsteroidEnemy.tscn")
 
-func _ready():
-	randomize()
-
 func _on_enemy_spawn_timer_timeout():
-	var rng = RandomNumberGenerator.new()
-	rng.randomize()
+	var enemy_position = Vector2(rand_range(-160, 670), rand_range(-90, 390))
 	
-	$Player/Path2D/PathFollow2D.offset = rng.randi_range(0, 3100)
-	var instance = enemy_asteroid.instance()
-	
-	instance.global_position = $Player/Path2D/PathFollow2D/Position2D.global_position
-	add_child(instance)
-	
-	
+	while enemy_position.x < 640 and enemy_position.x > -80 or enemy_position.y < 360 and enemy_position.y > -45:
+		enemy_position = Vector2(rand_range(-160, 670), rand_range(-90, 390))
+		
+	Global.instance_node(enemy_asteroid, enemy_position, self)
