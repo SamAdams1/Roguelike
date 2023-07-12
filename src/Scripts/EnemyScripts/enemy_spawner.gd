aaca536705bd35d87a5849fb11525eca163c4b89
SoccerSam16 <119465691+SoccerSam16@users.noreply.github.com>
2023-07-12 18:40:26 -0400

    Revert "Revert "Update enemy_spawner.gd""
    This reverts commit 7d9c5694e9f1546896d321af01ff572c4c0cc95c.

--- src/Scripts/EnemyScripts/enemy_spawner.gd
+++ src/Scripts/EnemyScripts/enemy_spawner.gd
@@ -2,10 +2,17 @@ extends Node
 
 var enemy_asteroid = preload("res://Scenes/Enemies/AsteroidEnemy.tscn")
 
+func _ready():
+	randomize()
+
 func _on_enemy_spawn_timer_timeout():
-	var enemy_position = Vector2(rand_range(-160, 670), rand_range(-90, 390))
+	var rng = RandomNumberGenerator.new()
+	rng.randomize()
 	
-	while enemy_position.x < 640 and enemy_position.x > -80 or enemy_position.y < 360 and enemy_position.y > -45:
-		enemy_position = Vector2(rand_range(-160, 670), rand_range(-90, 390))
-		
-	Global.instance_node(enemy_asteroid, enemy_position, self)
+	$Player/Path2D/PathFollow2D.offset = rng.randi_range(0, 3100)
+	var instance = enemy_asteroid.instance()
+	
+	instance.global_position = $Player/Path2D/PathFollow2D/Position2D.global_position
+	add_child(instance)
+	
+	
