7c38198d70f88581b285a1b6f3a910de1683dc9a
JacksonBair <Jacksonbair@gmail.com>
2023-07-13 15:13:43 -0500

    Basic friction, acceleration.
    re-did the movement to allow for acceleration and deceleration. Broke the model rotation

--- src/Scripts/Player.gd
+++ src/Scripts/Player.gd
@@ -1,8 +1,10 @@
 extends KinematicBody2D
 #player movement
 var velocity = Vector2.ZERO
-var playerSpeed = 500
-onready var animationTree = $AnimationTree
+var speed = 500
+var friction = 0.01
+var acceleration = 0.1
+#onready var animationTree = $AnimationTree
 
 #Stats
 var playerHealth = 50
@@ -15,23 +17,44 @@ var fireRate = 1
 var bulletSpeed = 500
 
 
-func _physics_process(_delta):
-	handle_input()
+func _physics_process(delta):
+	var input_velocity = Vector2.ZERO
+	# Check input for "desired" velocity
+	if Input.is_action_pressed("right"):
+		input_velocity.x += 1
+	if Input.is_action_pressed("left"):
+		input_velocity.x -= 1
+	if Input.is_action_pressed("down"):
+		input_velocity.y += 1
+	if Input.is_action_pressed("up"):
+		input_velocity.y -= 1
+	input_velocity = input_velocity.normalized() * speed
+
+	# If there's input, accelerate to the input velocity
+	if input_velocity.length() > 0:
+		velocity = velocity.linear_interpolate(input_velocity, acceleration)
+	else:
+		# If there's no input, slow down to (0, 0)
+		velocity = velocity.linear_interpolate(Vector2.ZERO, friction)
+	velocity = move_and_slide(velocity)
 	
-	if velocity != Vector2.ZERO:
-		animationTree.set("parameters/Move/blend_position", velocity)
+#func _physics_process(_delta):
+#	handle_input()
+#	
+#	if velocity != Vector2.ZERO:
+#		animationTree.set("parameters/Move/blend_position", velocity)
 	
 	
-func handle_input():
+#func handle_input():
 	#movement
-	velocity.x = int(Input.is_action_pressed("right")) - int(Input.is_action_pressed("left"))
-	velocity.y = int(Input.is_action_pressed("down")) - int(Input.is_action_pressed("up"))
+#	velocity.x = int(Input.is_action_pressed("right")) - int(Input.is_action_pressed("left"))
+#	velocity.y = int(Input.is_action_pressed("down")) - int(Input.is_action_pressed("up"))
 	
 	#fixes double speed on diagonals
-	velocity = velocity.normalized()
-	velocity = move_and_slide(velocity * playerSpeed)
+#	velocity = velocity.normalized()
+#	velocity = move_and_slide(velocity * playerSpeed)
 	
-#	look_at(get_global_mouse_position())
+	#look_at(get_global_mouse_position())
 	
 	#makes player look where moving
 	#MIGHT NEED IN THE FUTURE
