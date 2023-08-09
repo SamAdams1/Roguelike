0d2cdd964d5a595b855c4ddb7063c5b8400db907
MahmoudN77 <mt.nasereddin@gmail.com>
2023-08-09 17:14:57 -0500

    enemy shoot bullets

--- src/Scripts/EnemyScripts/enemy_shooter.gd
+++ src/Scripts/EnemyScripts/enemy_shooter.gd
@@ -2,6 +2,15 @@ extends "res://Scripts/EnemyScripts/enemy_core.gd"
 
 var is_in_void = false
 var enemyHealth = 15
+var nearestDistance = 100_000
+var nearestPlayer = null
+var autoAimUnlocked = false
+var autoBullet = preload("res://Scenes/autoBullet.tscn")
+var autoBulletWaitTimer = false
+var playerHealth = Global.playerHealth
+var autoAimLevel = 0
+onready var autoFireSound = $sounds/autoFireSound
+var bulletSpeed = Global.bulletSpeed
 
 onready var firingPositions := $FiringPositions
 var plBullet := preload("res://Scenes/EnemyBullet.tscn")
@@ -43,4 +52,37 @@ func fire():
 		var bullet := plBullet.instance()
 		bullet.global_position = child.global_position
 		get_tree().current_scene.add_child(bullet)
-		
+
+
+func findPlayer():
+	for player in Global.closeplayer:
+		var distance = self.global_position.distance_to(player.global_position)
+		if distance < nearestDistance:
+			nearestDistance = distance
+			nearestPlayer = player
+			Global.nearestPlayer = player
+			
+					
+func autoAim():
+	if autoAimUnlocked:
+		findPlayer()
+		if nearestPlayer != null and !autoBulletWaitTimer and enemyHealth > 0:
+			autoBulletWaitTimer = true
+			var counter = 0
+			while counter < autoAimLevel:
+				var burstTimer = true
+				if burstTimer:
+					var bullet_instance = autoBullet.instance()
+					bullet_instance.global_position = global_position
+					if is_instance_valid(nearestPlayer):
+						autoFireSound.play()
+						var direction = (self.global_position - nearestPlayer.global_position).normalized() * -1
+						bullet_instance.apply_impulse(Vector2(), direction * bulletSpeed)
+						get_parent().add_child(bullet_instance)
+					burstTimer = false
+				yield(get_tree().create_timer(0.1), "timeout")
+				burstTimer = true
+				counter += 1
+			
+			yield(get_tree().create_timer(fireRate), "timeout")
+			autoBulletWaitTimer = false
